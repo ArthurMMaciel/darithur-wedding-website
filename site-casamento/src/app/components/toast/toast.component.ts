7718d0c6b36a89555cead72bd9ba9a54d634a9a4
ArthurMaciel120119 <ra120119@uem.br>
2025-07-05 22:58:41 -0300

    update: finalized all sessions

---
+++ site-casamento/src/app/components/toast/toast.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-toast',
+  imports: [CommonModule],
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.scss'],
+  standalone: true,
+})
+export class ToastComponent {
+  @Input() message: string = '';
+  @Input() visible: boolean = false;
+}
--- site-casamento/src/app/modals/payment/payment-modal.component.ts
+++ site-casamento/src/app/modals/payment/payment-modal.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Copy, LucideAngularModule, X } from 'lucide-angular';
+import { ToastComponent } from '../../components/toast/toast.component';
 
 @Component({
   selector: 'app-payment-modal',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule],
+  imports: [CommonModule, ToastComponent],
   templateUrl: './payment-modal.component.html',
   styleUrls: ['./payment-modal.component.scss']
 })
@@ -13,7 +13,15 @@ export class PaymentModalComponent {
   @Input() paymentRandomKey: string = '';
   @Output() closed = new EventEmitter<void>();
 
+  showToast: boolean = false;
+
   public copyKey(): void {
     navigator.clipboard.writeText(this.paymentRandomKey)
+    this.showToast = true;
+    console.log(this.showToast);
+    setTimeout(() => {
+      this.showToast = false;
+      console.log(this.showToast);
+    }, 4000);
   }
 }
